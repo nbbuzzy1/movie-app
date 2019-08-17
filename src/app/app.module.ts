2f59da97e70008fb159fe401805d1866ac2a2145
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-08-17 07:12:10 -0400

    added initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieComponent } from './movie-list/movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieListComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
